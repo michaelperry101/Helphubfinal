3699df8a3cec8098779ac284f24d469fc67df527
michaelperry101 <michael.perry94@outlook.com>
2025-09-01 21:47:39 +0100

    Update ClientShell.jsx

--- components/ClientShell.jsx
+++ components/ClientShell.jsx
@@ -1,14 +1,14 @@
 "use client";
-import { SidebarProvider } from "./SidebarContext";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
+import ThemeProvider from "./ThemeProvider";
+import Navbar from "./Navbar";
 
 export default function ClientShell({ children }) {
   return (
-    <SidebarProvider>
-      <Header />
-      <Sidebar />
-      <div className="app-shell">{children}</div>
-    </SidebarProvider>
+    <ThemeProvider>
+      <div className="min-h-screen bg-white text-gray-900 dark:bg-neutral-950 dark:text-gray-100">
+        <Navbar />
+        <main className="mx-auto max-w-6xl px-4 py-8">{children}</main>
+      </div>
+    </ThemeProvider>
   );
 }
